ffca922dceca234247d53c32b6ad942ef5134e5e
Subbramanian Lakshmanan <subbu@esi-estech.com>
2017-05-13 17:52:09 -0500

    More Styling

--- static/js/simplescript.js
+++ static/js/simplescript.js
@@ -6,19 +6,24 @@ window.onload = function() {
 function initWordCloud(){
     console.log("initWordCloud");
     var words = [
-        {text: "Lorem", weight: 13},
-        {text: "Ipsum", weight: 10.5},
-        {text: "Dolor", weight: 9.4},
-        {text: "Sit", weight: 8},
-        {text: "Amet", weight: 6.2},
+        {text: "Android", weight: 13},
+        {text: "Java", weight: 10.5},
+        {text: "Javascript", weight: 9.4},
+        {text: "AngularJS", weight: 8},
+        {text: "IoT", weight: 6.2},
         {text: "Consectetur", weight: 5},
         {text: "Adipiscing", weight: 5},
     ];
 
     $('#mySkillSet').jQCloud(words, {
-        shape: 'rectangular',
+        shape: 'elliptic',
         width: 500,
-        height: 350
+        height: 350,
+        autoResize: true,
+        center: {x: 0.5, y:0.5},
+        delay: 5,
+        colors: ["#800026", "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#feb24c", "#fed976", "#ffeda0", "#ffffcc"],
+
     });
     console.log("initWordCloud");
 }
