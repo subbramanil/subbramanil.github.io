1fb7f47ea3ac5d4838338695c6f75c60f708bb19
Subbramanian Lakshmanan <subbu@esi-estech.com>
2017-05-13 17:40:49 -0500

    JS Wordcloud update

--- static/js/simplescript.js
+++ static/js/simplescript.js
@@ -1,3 +1,17 @@
-function testMe(){
-    console.log("Testing");
+window.onload = function() {
+  initWordCloud();
+};
+
+function initWordCloud(){
+    var words = [
+        {text: "Lorem", weight: 13},
+        {text: "Ipsum", weight: 10.5},
+        {text: "Dolor", weight: 9.4},
+        {text: "Sit", weight: 8},
+        {text: "Amet", weight: 6.2},
+        {text: "Consectetur", weight: 5},
+        {text: "Adipiscing", weight: 5},
+    ];
+
+    $('#mySkillSet').jQCloud(words);
 }
